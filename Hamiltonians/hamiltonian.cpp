4b22faae601a725e88e193523c3c819c7b2ab581
ageriksen <annikage@protonmail.com>
2021-02-11 18:16:23 +0100

    AddingNumericalDerivative

--- Hamiltonians/hamiltonian.cpp
+++ Hamiltonians/hamiltonian.cpp
@@ -5,3 +5,17 @@ Hamiltonian::Hamiltonian(System* system) {
     m_system = system;
 }
 
+Hamiltonian::numericDifferentiate()
+{
+
+	std::vector<class Particle*> particles = m_system->getParticles();
+
+	double wfcur = m_system->getWaveFunction()->evaluate(particles);
+	
+	for( int particle=0; 
+			particle < m_system->getNumberOfParticles(); 
+			particle++)
+	{
+		
+	}
+}
