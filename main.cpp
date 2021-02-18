a97a68d3718c8b3fcc23ebc9da264c01591dc100
bendkok <31344710+bendkok@users.noreply.github.com>
2021-02-18 19:47:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ int main() {
     // Seed for the random number generator
     int seed = 2020;
 
-    int numberOfDimensions[]    = {1,2};
-    int numberOfParticles[]     = {1,2,3}; //{1,10,100,500};
+    int numberOfDimensions[]    = {1};
+    int numberOfParticles[]     = {1}; //{1,10,100,500};
     int numberOfSteps           = (int) 1e6;
     double omega                = 1.0;          // Oscillator frequency.
-    double alpha                = 0.5;          // Variational parameter.
+    double alpha[]              = {0.45, 0.5, 0.55};          // Variational parameter.
     double stepLength           = 0.1;          // Metropolis step length.
     double equilibration        = 0.1;          // Amount of the total steps used
     // for equilibration.
@@ -33,15 +33,18 @@ int main() {
 
     for (int nDim : numberOfDimensions)
     {
-            for (int nPar : numberOfParticles)
+        for (int nPar : numberOfParticles)
         {
-            System* system = new System(seed);
-            system->setHamiltonian              (new HarmonicOscillator(system, omega));
-            system->setWaveFunction             (new SimpleGaussian(system, alpha));
-            system->setInitialState             (new RandomUniform(system, nDim, nPar));
-            system->setEquilibrationFraction    (equilibration);
-            system->setStepLength               (stepLength);
-            system->runMetropolisSteps          (numberOfSteps);
+            for (double nAlpha : alpha)
+            {
+                System* system = new System(seed);
+                system->setHamiltonian              (new HarmonicOscillator(system, omega));
+                system->setWaveFunction             (new SimpleGaussian(system, nAlpha));
+                system->setInitialState             (new RandomUniform(system, nDim, nPar));
+                system->setEquilibrationFraction    (equilibration);
+                system->setStepLength               (stepLength);
+                system->runMetropolisSteps          (numberOfSteps);
+            }
         }
     }
     
