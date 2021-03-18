326e183860a241321dea6096258074f1d48df7b5
bendkok <31344710+bendkok@users.noreply.github.com>
2021-03-18 20:59:58 +0100

    steepest descent implemntert (mulig feil)

--- sampler.h
+++ sampler.h
@@ -11,12 +11,16 @@ public:
     void printOutputToFile();
     void computeAverages();
     double getEnergy()          { return m_energy; }
+    double getDeltaPsi()        { return m_deltaPsi; }
+    double getDerivativePsiE()  { return m_derivativePsiE; }
 
 private:
     int                 m_numberOfMetropolisSteps = 0;
     int                 m_stepNumber = 0;
 	int					m_accepted = 0;
     double              m_energy = 0;
+    double              m_deltaPsi = 0;
+    double              m_derivativePsiE = 0;
     double              m_cumulativeEnergy = 0;
     std::string         m_output = "";
     std::vector<double> m_energies = std::vector<double>();
