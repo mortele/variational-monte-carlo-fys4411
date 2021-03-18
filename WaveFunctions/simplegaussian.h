eb33663cf1d62853be62202d93f9bdb9c39422d8
bendkok <31344710+bendkok@users.noreply.github.com>
2021-03-18 21:11:06 +0100

    fikset merge error

--- WaveFunctions/simplegaussian.h
+++ WaveFunctions/simplegaussian.h
@@ -4,7 +4,7 @@
 class SimpleGaussian : public WaveFunction {
 public:
     SimpleGaussian(class System* system, double alpha, double dt);
-    double evaluate(std::vector<class Particle*> particles, int particle);
+    double evaluate(std::vector<class Particle*> particles);
     double computeDerivative(std::vector<class Particle*> particles);
     double computeDoubleDerivative(double r2);
     // double computeDoubleDerivative(std::vector<class Particle*> particles, int particle);
