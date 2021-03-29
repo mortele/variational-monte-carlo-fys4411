9340316434d82977b11550865d59f2dabdae8ff0
ageriksen <annikage@protonmail.com>
2021-03-29 19:13:07 +0200

    removed print statements following program working.

--- main.cpp
+++ main.cpp
@@ -114,8 +114,6 @@ int main() {
                         double currEnergy = system->getSdRes()[0];
                         double currDeltaPsi = system->getSdRes()[1];
                         double currDerivativePsiE = system->getSdRes()[2];
-						std::cout << "gradient: " << std::endl;
-						std::cout << currDerivativePsiE - currEnergy*currDeltaPsi << std::endl;
                         alphaChange = eta*2*(currDerivativePsiE - currEnergy*currDeltaPsi);
                         alpha_guess -= alphaChange;
 
