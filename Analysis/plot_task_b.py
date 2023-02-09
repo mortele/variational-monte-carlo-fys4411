18892948e2759b639b39a767d83541963d743a94
hkve <haakon.kvernmoen@gmail.com>
2023-02-09 18:19:51 +0100

    Added optional save option and added xy-labels

--- Analysis/plot_task_b.py
+++ Analysis/plot_task_b.py
@@ -7,10 +7,10 @@ import cpp_utils
 
 import subprocess
 
-def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11)):
+def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11), save=False):
     alphas = np.linspace(*alpha_range)
     Ns = [1,10,100,500]
-
+    
     if not cpp_utils.dataPath(filename).exists():
         total = len(Ns)*len(alphas)
         k = 1
@@ -23,8 +23,8 @@ def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11
     df = cpp_utils.vmcLoad(filename=filename)
 
     c = plot_utils.colors
-    fig, ax = plt.subplots()
     for i, N in enumerate(Ns):
+        fig, ax = plt.subplots()
         df_N = df[ df.Particles == N ]
         E, E_var, alpha, MCCs = df_N.Energy.to_numpy(), df_N.Energy_var.to_numpy(), df_N["WF1"].to_numpy(), df_N["Metro-steps"].to_numpy()
         Error = np.sqrt(E_var/MCCs) # np.sqrt(E_var)
@@ -32,15 +32,13 @@ def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11
         ax.plot(alpha, E+Error, c=c[i], ls="--")
         ax.plot(alpha, E-Error, c=c[i], ls="--")
         ax.legend()
+        ax.set(xlabel=r"$\alpha$", ylabel="E $[\hbar \omega]$")
         print(f"Minimum energy at alpha = {alpha[np.argmin(E)]}")
+        
+        if save:
+            plot_utils.save(filename.replace(".txt",f"{N}_plot"))
+        plt.show()
 
-        # y_lim_old = ax.get_ylim()
-        # y_lim_new = (0.75*np.min(E), y_lim_old[1])
-        # ax.set_ylim(y_lim_new)
-
-    # plot_utils.save(filename.replace(".txt",f"{N}_plot"))
-    ax.set_yscale("log")
-    plt.show()
 
 if __name__ == "__main__":
     plot_simple_HO()
