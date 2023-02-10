71d9590bd365eb395a933c346ac3c10d00d72c73
hkve <haakon.kvernmoen@gmail.com>
2023-02-10 14:19:26 +0100

    Now with not stupid parameters. 1D and 2D is now properly variational

--- Analysis/plot_task_b.py
+++ Analysis/plot_task_b.py
@@ -7,16 +7,19 @@ import cpp_utils
 
 import subprocess
 
-def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11), save=False):
+def plot_simple_HO(filename="simple_HO", omega=1.0, alpha_range=(0.1,1.1, 11), save=False):
     alphas = np.linspace(*alpha_range)
     Ns = [1,10,100,500]
-    
+    stepLengths = [0.1, 0.1, 0.5, 1.0]
+    D = 2
+
+    filename = f"{filename}_{D}D.txt"
     if not cpp_utils.dataPath(filename).exists():
         total = len(Ns)*len(alphas)
         k = 1
-        for N in Ns:
+        for N, stepLength in zip(Ns, stepLengths):
             for i, alpha in enumerate(alphas):
-                cpp_utils.vmcRun(filename=filename, N=N, alpha=alpha)
+                cpp_utils.vmcRun(D=D, filename=filename, N=N, alpha=alpha, stepLength=stepLength)
                 print(f"Done {k}/{total}...")
                 k += 1
 
@@ -27,7 +30,7 @@ def plot_simple_HO(filename="simple_HO.txt", omega=1.0, alpha_range=(0.1,1.1, 11
         fig, ax = plt.subplots()
         df_N = df[ df.Particles == N ]
         E, E_var, alpha, MCCs = df_N.Energy.to_numpy(), df_N.Energy_var.to_numpy(), df_N["WF1"].to_numpy(), df_N["Metro-steps"].to_numpy()
-        Error = np.sqrt(E_var/MCCs) # np.sqrt(E_var)
+        Error = np.sqrt(E_var/MCCs)
         ax.plot(alpha, E, c=c[i], label=f"{N =}", marker="o")
         ax.plot(alpha, E+Error, c=c[i], ls="--")
         ax.plot(alpha, E-Error, c=c[i], ls="--")
