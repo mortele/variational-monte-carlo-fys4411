a4aaf8d4aa166b7bbc606debbdb1169c7cd1dede
hkve <haakon.kvernmoen@gmail.com>
2023-02-12 16:44:16 +0100

    All base cases done and plotted, being nice and variational. This should/could be a table

--- Analysis/plot_task_b.py
+++ Analysis/plot_task_b.py
@@ -7,11 +7,10 @@ import cpp_utils
 
 import subprocess
 
-def plot_simple_HO(filename="simple_HO", omega=1.0, alpha_range=(0.1,1.1, 11), save=False):
+def plot_simple_HO(filename="simple_HO", D=1, omega=1.0, alpha_range=(0.1,1.1, 11), save=False):
     alphas = np.linspace(*alpha_range)
     Ns = [1,10,100,500]
     stepLengths = [0.1, 0.1, 0.5, 1.0]
-    D = 2
 
     filename = f"{filename}_{D}D.txt"
     if not cpp_utils.dataPath(filename).exists():
@@ -26,22 +25,28 @@ def plot_simple_HO(filename="simple_HO", omega=1.0, alpha_range=(0.1,1.1, 11), s
     df = cpp_utils.vmcLoad(filename=filename)
 
     c = plot_utils.colors
+    fig, ax = plt.subplots()
     for i, N in enumerate(Ns):
-        fig, ax = plt.subplots()
         df_N = df[ df.Particles == N ]
         E, E_var, alpha, MCCs = df_N.Energy.to_numpy(), df_N.Energy_var.to_numpy(), df_N["WF1"].to_numpy(), df_N["Metro-steps"].to_numpy()
         Error = np.sqrt(E_var/MCCs)
         ax.plot(alpha, E, c=c[i], label=f"{N =}", marker="o")
         ax.plot(alpha, E+Error, c=c[i], ls="--")
         ax.plot(alpha, E-Error, c=c[i], ls="--")
-        ax.legend()
-        ax.set(xlabel=r"$\alpha$", ylabel="E $[\hbar \omega]$")
         print(f"Minimum energy at alpha = {alpha[np.argmin(E)]}")
         
-        if save:
-            plot_utils.save(filename.replace(".txt",f"{N}_plot"))
-        plt.show()
+    
+    ax.legend(ncol=4, bbox_to_anchor=(1.05, 1.15))
+    ax.set(xlabel=r"$\alpha$", ylabel="E $[\hbar \omega]$")
+    ax.set_yscale("log")
+    ylim = ax.get_ylim()
+    ax.vlines(x=0.5, ymin=ylim[0], ymax=ylim[1], color="gray", ls="--", alpha=0.4)
+    if save:
+        plot_utils.save(filename.replace(".txt",f"_plot"))
+    plt.show()
 
 
 if __name__ == "__main__":
-    plot_simple_HO()
+    plot_simple_HO(D=1, save=True)
+    plot_simple_HO(D=2, save=True)
+    plot_simple_HO(D=3, save=True)
