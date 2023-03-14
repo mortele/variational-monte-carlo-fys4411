3fc1107591a51195209ab5e9161f4fc0a55ec768
hkve <haakon.kvernmoen@gmail.com>
2023-03-14 15:58:34 +0100

    Added evalute_w which uses closed form Psi'2/Psi2 expr instead of double evalute call

--- WaveFunctions/simplegaussian.h
+++ WaveFunctions/simplegaussian.h
@@ -9,6 +9,7 @@ class SimpleGaussian : public WaveFunction
 public:
     SimpleGaussian(double alpha);
     double evaluate(std::vector<std::unique_ptr<class Particle>> &particles);
+    double evaluate_w(int proposed_particle_idx, class Particle &proposed_particle, class Particle &old_particle, std::vector<std::unique_ptr<class Particle>> &particles);
     double computeParamDerivative(std::vector<std::unique_ptr<class Particle>> &particles, int parameterIndex);
     double computeDoubleDerivative(std::vector<std::unique_ptr<class Particle>> &particles);
     void quantumForce(Particle &particle, std::vector<double> &force);
