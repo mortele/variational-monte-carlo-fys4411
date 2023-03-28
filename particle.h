67f124087c17de95f969e2f67ca5d639834d7a16
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2023-03-28 14:32:16 +0200

    Return a reference to position (#15)

--- particle.h
+++ particle.h
@@ -5,7 +5,7 @@ class Particle {
 public:
     Particle(const std::vector<double>& position);
     void adjustPosition(double change, unsigned int dimension);
-    std::vector<double> getPosition() { return m_position; }
+    std::vector<double> &getPosition() { return m_position; }
     unsigned int getNumberOfDimensions() { return m_numberOfDimensions; }
 
 private:
